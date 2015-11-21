c2615c7ec0c7104372988569a4e1055c40250fdc
zhihao wang <hao@opentown.cn>
2015-11-21 11:45:33 +0800

    add search method

--- test/freeSearch.js
+++ test/freeSearch.js
@@ -4,16 +4,21 @@ function loop() {
     wechat
         .waitConsole().then(function(input) {
             if (input != 'exit') {
-                this.search(input).then(function(result) {
-                    console.log(result);
-                    if (result == false) {
-                        console.log('no matched.')
-                    } else {
-                        result.click();
-                    }
-                    loop();
-                });
+                this.search(input)
+                    .then(function(isExisting) {
+                        //console.log(this);
+                        if(isExisting){
+                          console.log('找到该用户，可以自动发信了。');
+                        }else{
+                          console.log('用户不存在，请尝试加ta。');
+                        }
+                        loop();
+                    });
+            }else{
+              this.end();
             }
         })
 }
+
+
 loop();
